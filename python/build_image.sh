27f7cb2257e1da8f3f54aac4cd3fb7e354944eda
胡一帆 <yifan.hu@geely.com>
2019-11-27 09:50:07 +0800

    xxxxx

--- python/build_image.sh
+++ python/build_image.sh
@@ -4,7 +4,7 @@ image_tag="0.2.1"
 full_image_name=${image_name}:${image_tag}
 
 cd "$(dirname "$0")"
-docker build -t "${full_image_name}" .
+docker build -f storage-initializer.Dockerfile -t "${full_image_name}" .
 docker push "$full_image_name"
 
 # Output the strict image name (which contains the sha256 image digest)
